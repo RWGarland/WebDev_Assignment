90047c0111ff9e092b8f0ec06aef784314ef26ae
Rupert Garland <rupertwilliamgarland@gmail.com>
2017-11-22 16:01:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,246 +1,45 @@
 
-<script type="text/JavaScript">
+window.addEventListener("load", init);
 
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("load", function() {
-    if (xhr.readyState == 4 && xhr.status == 200){
-        var elem = document.querySelector("#result");
-        elem.textContent = xhr.responseText;
+/*function init(){
+    
+    document.querySelector("submit").addEventListener("click", pack);
+    
+  
+    document.querySelector(".loading").style.display = "none";
+    document.querySelector(".success").style.display = "none";
+    document.querySelector(".error").style.display = "none";
+}
+
+function pack(evt){
+    fields_ok = true;
+    
+    document.querySelector(".searchva").style.display = "none";
+    document.querySelector(".loading").style.display = "block";
+    
+    var s = document.querySelector("#search").value.trim();
+    
+    if (fields_ok) {
+        var data = "search=" + encodeURIComponent(s);
+        console.log(data);
+*/        
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", function() {
+            if (xhr.readyState == 4 && xhr.status == 200){
+           var elem = document.querySelector("#result");
+                elem.textContent = xhr.responseText;
+            
+            
     }
 });
-xhr.open("GET", "http://www.test.com/item.php?id=123", true);
-xhr.send();
-
-
-
-{
-    "records": [
-        {
-            "pk": 19781,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "R W Thrupp",
-                "museum_number": "S.149:284-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1492842007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O233545",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CA6926",
-                "latitude": "",
-                "date_text": "ca. late 19th century",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 24544,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "R W Thrupp",
-                "museum_number": "S.149:282-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1492822007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O233539",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CA6924",
-                "latitude": "",
-                "date_text": "ca. late 19th century",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 23811,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "Unknown",
-                "museum_number": "E.982-2002",
-                "object": "Poster",
-                "museum_number_token": "e9822002",
-                "location": "Prints & Drawings Study Room, level C, case Y, shelf 69",
-                "place": "United Kingdom",
-                "object_number": "O233533",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "PDP",
-                "sys_updated": "",
-                "slug": "vanity-fair-kd-langs-edge-poster",
-                "primary_image_id": "2015HN0754",
-                "latitude": "",
-                "date_text": "1993",
-                "title": "Vanity Fair. k.d. lang's Edge"
-            }
-        },
-        {
-            "pk": 14446,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "London Stereoscopic and Photographic Company",
-                "museum_number": "S.146:298-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1462982007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O233522",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB4117",
-                "latitude": "",
-                "date_text": "late 19th century\nca. 2000s",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 19195,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "London Stereoscopic and Photographic Company",
-                "museum_number": "S.146:292-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1462922007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O233508",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB4112",
-                "latitude": "",
-                "date_text": "late 19th century\nca. 2000s",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 38800,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "London Stereoscopic and Photographic Company",
-                "museum_number": "S.146:291-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1462912007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O233507",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB4111",
-                "latitude": "",
-                "date_text": "late 19th century\nca. 2000s",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 14667,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "London Stereoscopic and Photographic Company",
-                "museum_number": "S.146:289-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1462892007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O233505",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB4109",
-                "latitude": "",
-                "date_text": "late 19th century\nca. 2000s",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 38719,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "W&D Downey Photographers",
-                "museum_number": "S.149:168-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1491682007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O232334",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB3538",
-                "latitude": "",
-                "date_text": "ca. late 19th century",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 43353,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "London Stereoscopic and Photographic Company",
-                "museum_number": "S.145:663-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1456632007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O232336",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB3320",
-                "latitude": "",
-                "date_text": "19th century",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        },
-        {
-            "pk": 31631,
-            "model": "collection.museumobject",
-            "fields": {
-                "artist": "Walker, C.B.",
-                "museum_number": "S.145:660-2007",
-                "object": "Photograph",
-                "museum_number_token": "s1456602007",
-                "location": "In Storage",
-                "place": "Great Britain",
-                "object_number": "O232326",
-                "rights": 3,
-                "longitude": "",
-                "collection_code": "T&P",
-                "sys_updated": "",
-                "slug": "guy-little-theatrical-photograph-photograph",
-                "primary_image_id": "2009CB3317",
-                "latitude": "",
-                "date_text": "19th century",
-                "title": "Guy Little Theatrical Photograph"
-            }
-        }
-    ],
-    "meta": {
-        "group_details": [],
-        "cluster_counts": {},
-        "clusters": [],
-        "result_count": 1183374
+        xhr.open("GET", "http://itsuite.it.brighton.ac.uk/rg425/ci227/assignment/search.php", true);
+        xhr.send();
     }
+    evt.preventDefault();
+    
 }
-</script>
+
+
+
+

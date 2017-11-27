453e585de6e393b570ea26a04db67e11b0adcec3
Rupert Garland <rupertwilliamgarland@gmail.com>
2017-11-27 19:40:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,73 +3,97 @@ window.addEventListener("load", init);
 
 function init(){
     
-    document.querySelector("#submit").addEventListener("click", pack);
-    document.querySelector("#back2search").addEventListener("click", restart);
-    
+    document.querySelector("#submitButton").addEventListener("click", getApi);
   
-    document.querySelector(".loading").style.display = "none";
-    document.querySelector(".api").style.display = "none";
+ //   document.querySelector(".loading").style.display = "none";
     
-    
-    document.querySelector("#required").style.display = "none";
 }
 
-function pack(evt){
-    fields_ok = true;
+function getApi(evt){
     
-    document.querySelector(".searchva").style.display = "none";
-    document.querySelector(".loading").style.display = "block";
-    
-    document.querySelector("#required").style.display = "none";
-    
-    var s = document.querySelector("#search").value.trim();
-    
+    var checkField = true;
+    var s = document.querySelector("#submitBox").value.trim();
+   // document.querySelector(".loading").style.display = "block";
+
     if (s == 0){
-		fields_ok = false;
-		document.querySelector(".searchva").style.display = "block";
-		document.querySelector(".loading").style.display = "none";
-		document.querySelector("#required").style.display = "block";
+        checkField = false;
+        var a = document.querySelector("#submitBox");
+        a.setAttribute("placeholder", "enter search");
     }
-      /*if (fields_ok) {
-      var data = "search=" + encodeURIComponent(s);
-        console.log(data);
-        */
-        var url = "http://itsuite.it.brighton.ac.uk/rg425/assignment/search.php"; 
-          
+    
+    if (checkField){
+    
         var xhr = new XMLHttpRequest();
-          
-          xhr.addEventListener("load", function() {
-              if (xhr.readystate == 4){
-                  document.querySelector("#result").style.desplay = "none";
-                  
-                  if(xhr.status == 200) {
-                      console.log(xhr);
-                      console.log(xhr.responseText);
-                      document.querySelector(".searchva").style.display = "none";
-                      document.querySelector(".api").style.display = "block";
-                  }
-              }
+          xhr.addEventListener("load", function(){
+             if(xhr.readyState ===4 && xhr.status===200)
+            {
+                console.dir(xhr)
+                console.log(xhr.responseText);
+                response = JSON.parse(xhr.responseText);
+                
+                console.log("total results: " + responce.meta.result_count);
+                console.log("");
+                
+                log();
+                show();
+            } 
           });
 
-       /* xhr.addEventListener("load", function() {
-            if (xhr.readyState == 4 && xhr.status == 200){
-           var elem = document.querySelector("#result");
-                elem.textContent = xhr.responseText;
-            
-
-        }*/
-    
-          
-          xhr.open("POST", url, true);
-          xhr.setRequestHeader("content-type", "application/x-www-form-urlendcoded");
-          xhr.send(data);
-           /* xhr.open("GET", "http://itsuite.it.brighton.ac.uk/rg425/ci227/assignment/search.php", true);
-        xhr.send(data);*/
+        xhr.open("GET",  getURL(), true);
+        xhr.send();
+    }
     evt.preventDefault();
-    
 }
 
-function restart(){	
-	document.querySelector(".searchva").style.display = "block";
-	document.querySelector(".api").style.display = "none";
+function getURL(){
+    
+    var searched = document.querySelector("#submitBox");	
+	var searchURL = "http://www.vam.ac.uk/api/json/museumobject/search?q=";
+	var searchArray = searched.value.split(" ");
+	console.log("Search value: " + searched.value);
+	console.log(searchArray);
+    
+    if(searchArray != 0){
+		for(var i = 0; i < searchArray.length; i++){
+			searchURL += searchArray[i] + "+";
+		}
+		console.log(searchURL);
+		return searchURL;		
+	}
+	return "http://www.vam.ac.uk/api/json/museumobject";
+}
+
+function log(){
+	var length = 15;
+	
+	for(var i = 0; i < length; i++){
+		
+		var r = document.createElement("div");
+		var rTitle = document.createElement("p");
+		var rDate = document.createElement("p");
+		
+		rTitle.textContent = response.records[i].fields.title;
+		rDate.textContent = response.records[i].fields.date_text;
+		
+		r.appendChild(rTitle);
+		r.appendChild(rDate);
+		
+		var t = document.querySelector("#results");
+		t.appendChild(r);
+		
+		
+		
+		
+		console.log("Result: " + (i+1));
+		console.log("Title: " + response.records[i].fields.title);
+		console.log("Object: " + response.records[i].fields.object);
+		console.log("Artist: " + response.records[i].fields.artist);
+		console.log("Date: " + response.records[i].fields.date_text);
+		console.log("");
+	}
+}
+
+function show(){
+    
+    
 }
